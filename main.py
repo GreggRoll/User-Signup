441457d8070025acde5b0983fbce31cdff76f182
Greg Adams <adamsg_53@yahoo.com>
2018-09-21 19:45:47 -0400

    trying to fix this

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+need to create a webpage
+has a <h2><er>Signup
+has a <label> username with input
+    returns username to next page and says "Welcome, username"
+has a <label> password
+has a label verify password
+    if verify != password then it states passwords don't match
+has a email field
+
+submit query box
+'''
