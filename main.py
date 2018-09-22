5075954d6e7ba4d2f3eea59daaaf59baa5f53e22
Greg Adams <adamsg_53@yahoo.com>
2018-09-21 21:12:05 -0400

    sorta fixed

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
-'''
-need to create a webpage
-has a <h2><er>Signup
-has a <label> username with input
-    returns username to next page and says "Welcome, username"
-has a <label> password
-has a label verify password
-    if verify != password then it states passwords don't match
-has a email field
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
 
-submit query box
-'''
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/signup')
+def index():
+    return render_template('input.html')
+
+@app.route('/signup', methods=['POST'])
+def validate_form():
+
+    username = request.form['username']
+    username_error = ''
+
+    password = request.form['password']
+    password_error = ''
+
+    verify_password = request.form['verify_password']
+    verify_error = ''
+
+    email = request.form['email']
+    email_error = ''
+
+    if username == '':
+        username_error = "That's not a valid username"
+        username = ''
+    if password == '':
+        password_error = "That's not a valid password"
+        password == ''
+    if password != verify_password:
+        verify_error = "Passwords don't match"
+    if '@' and '.' not in email:
+        email_error = "That's not a valid email"
+        email = ''
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect('/valid_hello_{username}'.format(username))
+
+    else:
+        return render_template('input.html',
+            username_error=username_error,
+            password_error = password_error,
+            verify_error = verify_error,
+            email_error = email_error
+        )
+
+@app.route("/valid_hello", methods=['POST'])
+def valid_hello():
+    user_name = request.form['username']
+    return render_template('hello.html', name=user_name)
+    
+app.run()
